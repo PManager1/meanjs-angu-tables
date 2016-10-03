9527516c40cca8f3d3cb078e0b2d5ec85656c218
jp-ca <jp_ca@ymail.com>
2016-10-03 17:32:58 -0400

    single selection got it working -  md-row md-select=property

--- modules/properties/client/config/properties.client.routes.js
+++ modules/properties/client/config/properties.client.routes.js
@@ -17,7 +17,7 @@
       .state('properties.list', {
         url: '',
         templateUrl: 'modules/properties/client/views/list-properties.client.view.html',
-        controller: 'PropertiesListController',
+        // controller: 'PropertiesListController',
         controllerAs: 'vm',
         css: 'css/md-data-table.min.css',
         data: {
